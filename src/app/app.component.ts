f3d15f8bcbd4fcb4f06aaa52f42bc806a5a84419
Daylin Mejia <daylinmejia24@gmail.com>
2024-10-20 18:01:01 +0200

    sprint 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "./frontend/hero/hero.component";
+import { NavbarComponent } from "./frontend/navbar/navbar.component";
+import { CountriesComponent } from "./frontend/countries/countries.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent, NavbarComponent, CountriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
